4504e9fad9af4a42dcf04989adc3dd7f0b7149b9
Joel <joeldomingueztapia@gmail.com>
2026-05-11 09:51:26 -0700

    Agregar codigo para Taranis ES900RX y tutorial README

---
+++ Centinela_Brazo/Pruebas/taranis_crsf_elrs/README.md
@@ -0,0 +1,3 @@
+# Tutorial para conectar Taranis ES900TX a ES900RX
+
+![Receiver Wiring - ELRS Documentation](https://www.expresslrs.org/assets/images/EPWiring.png)
---
+++ Centinela_Brazo/Pruebas/taranis_crsf_elrs/taranis_crsf_esp32_elrs.ino
@@ -0,0 +1,46 @@
+// Este codigo fue probado en Arduino IDE
+
+// Utiliza la libreria CRSF for Arduino de Cassandra Robinson. Descargarla antes de ejecutar este script.
+
+// TO-DO LIST:
+//      -> Corregir el orden de los canales, se encuentra desordenado.
+
+#include <CRSFforArduino.hpp>
+
+CRSFforArduino *crsf = nullptr;
+
+void onReceiveRcChannels(serialReceiverLayer::rcChannels_t *rcData);
+
+void setup() {
+  Serial.begin(115200);
+  delay(1000);
+
+  crsf = new CRSFforArduino(&Serial2, 16, 17); // RX=16, TX=17
+
+  if (crsf->begin() == true) {
+    Serial.println("CRSF OK");
+    crsf->setRcChannelsCallback(onReceiveRcChannels);
+  } else {
+    Serial.println("CRSF FALLO");
+    crsf->end();
+    delete crsf; 
+    crsf = nullptr;
+  }
+}
+
+void loop() {
+  if (crsf != nullptr) {
+    crsf->update();
+  }
+}
+
+void onReceiveRcChannels(serialReceiverLayer::rcChannels_t *rcData) {
+  static uint32_t lastPrint = millis();
+  if (millis() - lastPrint >= 100) {
+    lastPrint = millis();
+    Serial.print("CH1:"); Serial.print(crsf->rcToUs(rcData->value[0]));
+    Serial.print(" CH2:"); Serial.print(crsf->rcToUs(rcData->value[1]));
+    Serial.print(" CH3:"); Serial.print(crsf->rcToUs(rcData->value[2]));
+    Serial.print(" CH4:"); Serial.println(crsf->rcToUs(rcData->value[3]));
+  }
+}
